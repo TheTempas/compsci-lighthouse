2b0f83c75c921507238030fdfa23ee9104a1cfb3
Nicole <nicole.tempas@gmail.com>
2018-08-01 16:34:58 +0000

    Classes & Instances exercise

---
+++ pizza.js
@@ -0,0 +1,26 @@
+class Pizza {
+
+  constructor(size, crust) {
+    this.size = size;
+    this.crust = crust;
+    this.toppings = ["cheese"];
+  }
+
+  addTopping(topping) {
+    this.toppings.push(topping);
+
+  }
+}
+
+let pizza1 = new Pizza();
+console.log(pizza1.toppings);
+pizza1.addTopping("mushrooms");
+pizza1.addTopping("peppers");
+console.log(pizza1.toppings);
+
+let pizza2 = new Pizza();
+console.log(pizza2.toppings);
+pizza2.addTopping("more cheese");
+console.log(pizza2.toppings);
+
+let pizza = new Pizza('large', 'thin')
